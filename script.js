f119bd851e0b699c2dd1df8457c41c9b3fb93ea2
bishop-of-intensity <79968439+bishop-of-intensity@users.noreply.github.com>
2024-09-17 14:55:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $('.buy-popup-btn-container').click(function() {
     if(!$('.buy-popup-container').hasClass('visible')) {
         $('.buy-popup-container').addClass('visible');
+        $('html').addClass('overflow-none');
     }
 });
 
 $('.close-sign').click(function() {
     $('.buy-popup-container').removeClass('visible');
+    $('html').removeClass('overflow-none');
 });
